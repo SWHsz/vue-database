84ab73b4fd5319285306e9172a1c4ddac963f767
SWHsz <568877201@qq.com>
2023-06-04 19:29:13 +0800

    modified:   build/utils.js

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../' // 解决css中引入图片路径不对的问题
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
