2c18d7780e6b3fc11f47e9cd9918f0dacc1b428d
SWHsz <568877201@qq.com>
2023-06-04 23:25:57 +0800

    modified:   src/views/user/graph.vue

--- src/views/user/graph.vue
+++ src/views/user/graph.vue
@@ -2,6 +2,8 @@
     <div>
     <span v-show="loadingLoad">正在加载</span>
     <img :src="picurl" alt="网络干扰结构分析" style="width:100%;height:100%;" v-show="!loadingLoad"/>
+    <el-input v-model="loc" placeholder="请输入下载名称.png"></el-input>
+    <el-button type="primary" @click="download">下载</el-button>
     </div>
 </template>
 
@@ -13,6 +15,7 @@ export default {
         return {
             picurl: "",
             loadingLoad: false,
+            loc:""
         }
     },
     mounted() {
@@ -39,6 +42,19 @@ export default {
                 console.log(err)
                 that.loadingLoad = false
             })
+        },
+        download(){
+            
+            let link = document.createElement('a');
+        link.href = this.picurl;
+        if (this.loc == "") {
+            this.loc = "网络干扰结构分析.png"
+        }
+        else if(this.loc != "" && this.loc.indexOf(".png") == -1){
+            this.loc = this.loc + ".png"
+        }
+        link.download = this.loc;
+        link.click();
         },
         arrayBufferToBase64(buffer) {
             var binary = '';
